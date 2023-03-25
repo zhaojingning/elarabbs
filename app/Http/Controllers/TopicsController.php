8881d7c5b1d44f89934e4aebb68f713e4aee005b
zhaojingning <784955168@qq.com>
2023-03-25 18:20:17 +0800

    修复 XSS 注入漏洞

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -36,9 +36,9 @@ class TopicsController extends Controller
 		return view('topics.create_and_edit', compact('topic', 'categories'));
 	}
 
-	public function store(TopicRequest $request)
+	public function store(TopicRequest $request, Topic $topic)
 	{
-		$topic = Topic::create($request->all());
+		$topic->fill($request->all());
 		$topic->user_id = Auth::id();
 		$topic->save();
 		return redirect()->route('topics.show', $topic->id)->with('message', '帖子创建成功');
