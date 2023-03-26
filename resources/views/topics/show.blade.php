62af8581e75cb6ce8c143ca22d8e8fcf1bbce93a
zhaojingning <784955168@qq.com>
2023-03-26 14:50:54 +0800

    回复列表

--- resources/views/topics/show.blade.php
+++ resources/views/topics/show.blade.php
@@ -26,7 +26,7 @@
     </div>
 
     <div class="col-lg-9 col-md-9 col-sm-12 col-xs-12 topic-content">
-      <div class="card ">
+      <div class="card">
         <div class="card-body">
           <h1 class="text-center mt-3 mb-3">
             {{ $topic->title }}
@@ -44,7 +44,7 @@
           </div>
 
           @can('update', $topic)
-          <div class="operate">
+            <div class="operate">
               <hr>
               <a href="{{ route('topics.edit', $topic->id) }}" class="btn btn-outline-secondary btn-sm" role="button">
                 <i class="far fa-edit"></i> 编辑
@@ -63,6 +63,15 @@
 
         </div>
       </div>
+
+      {{-- 用户回复列表 --}}
+      <div class="card topic-reply mt-4">
+          <div class="card-body">
+              @include('topics._reply_box', ['topic' => $topic])
+              @include('topics._reply_list', ['replies' => $topic->replies()->with('user')->get()])
+          </div>
+      </div>
+
     </div>
   </div>
 @stop
